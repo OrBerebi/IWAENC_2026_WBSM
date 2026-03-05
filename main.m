c04b0a761bfda1dae55077aa3bf850e97607648f
Or Berebi <76527745+OrBerebi@users.noreply.github.com>
2026-03-05 14:47:46 +0200

    cleanup

--- main.m
+++ main.m
@@ -24,76 +24,14 @@ arraysetup.fs           = 48e3;
     BSM_toolbox.GetArrayPositions(arraysetup.arrayType, arraysetup.M, 0);
 
 fprintf('Pre-loading data structures...\n');
-dummy_params = init_params_local(1, base_path, figures_save_folder);
+dummy_params    = init_params_local(1, base_path, figures_save_folder);
 dummy_params.fs = arraysetup.fs;
-data_static      = load_and_prepare_data_local(dummy_params); 
-
-    % Frequency cutoff logic for MLS
-f_vec = linspace(0, round(data_static.fs/2), round(data_static.nFFT/2)+1);
-f_cutoff = 1.5e3;
-
-f_cutoff_high = 8e3;
-
-[~,idx_cutoff] = min(abs(f_vec - f_cutoff));
-[~,idx_cutoff_high] = min(abs(f_vec - f_cutoff_high));
-
-% this one calculate the BSM coeefs and evaluate them
-%res_out    = anechoic_analysis_FOV_BSM_v3(beta, base_path,figures_save_folder,arraysetup);
+data_static     = load_and_prepare_data_local(dummy_params); 
 
 plot_anechoic_analysis = true;
 res_out  = anechoic_analysis_FOV_BSM_v5(beta,arraysetup, base_path, figures_save_folder, data_static, plot_anechoic_analysis);
 
 
-
-% %% Simulate array response in a room
-% type_str = 'small';
-% roomsetup               = setup_room_params(type_str);
-% roomsetup.fs            = 48e3;
-% roomsetup.N_PW          = 32;
-% 
-% roomsetup.sourcePos     = [roomsetup.roomDim(1)-0.5, roomsetup.roomDim(2)-0.5, roomsetup.arrayPos(3)];
-% res_out_room_babble     = room_auralisation_v3(roomsetup,arraysetup);
-% 
-% 
-% src_r = roomsetup.critical_distance;
-% src_phi   = deg2rad(60);
-% src_theta = deg2rad(90);  
-% [srcPosx, srcPosy, srcPosz] = s2c(src_theta, src_phi, src_r);
-% roomsetup.sourcePos     = [srcPosx, srcPosy, srcPosz] + roomsetup.arrayPos;
-% res_out_room_1 = room_auralisation_v3(roomsetup,arraysetup);
-% 
-% src_r = roomsetup.critical_distance*2;
-% src_phi   = deg2rad(80);
-% src_theta = deg2rad(90);  
-% [srcPosx, srcPosy, srcPosz] = s2c(src_theta, src_phi, src_r);
-% roomsetup.sourcePos     = [srcPosx, srcPosy, srcPosz] + roomsetup.arrayPos;
-% res_out_room_2 = room_auralisation_v3(roomsetup,arraysetup);
-% 
-% 
-% src_r = roomsetup.critical_distance*0.7;
-% src_phi   = deg2rad(-40);
-% src_theta = deg2rad(90);  
-% [srcPosx, srcPosy, srcPosz] = s2c(src_theta, src_phi, src_r);
-% roomsetup.sourcePos     = [srcPosx, srcPosy, srcPosz] + roomsetup.arrayPos;
-% res_out_room_3 = room_auralisation_v3(roomsetup,arraysetup);
-% 
-% 
-% 
-% 
-% % this one generate the BRIRs
-% roomsetup.sig_path      = base_path +"/dry_signals/casta_short.wav";
-% roomsetup.HRTF_path     = base_path + "/HRTF/HRIR_L2702_koln.sofa";
-% res_out_room_sim_1      = bsm_binaural_renderer_V3(res_out.BSMobj,res_out.c_TBSM,res_out.c_BSM,res_out_room_1,roomsetup);
-% 
-% 
-% % this one renderes the binaural signals with the BRIRs
-% sig_path = roomsetup.sig_path;
-% save_path = brirs_path;
-% brir_flag = false;
-% save_room_auralisation(res_out_room_sim_1,roomsetup.fs,save_path,sig_path,brir_flag)
-% brir_flag = true;
-% save_room_auralisation(res_out_room_sim_1,roomsetup.fs,save_path,sig_path,brir_flag)
-% 
 
 
 
